508adf4c7b9132869fc3070c5bbecb5debb486a9
yvan <chavez.yvan@gmail.com>
2020-05-22 17:57:35 -0500

    sweet alert agregado

--- semana7/dia2/02-peliculas.js
+++ semana7/dia2/02-peliculas.js
@@ -1,36 +1,75 @@
 const dibujarPeliculas = ({ results }) => {
- results.forEach((peli) => {
-  $("#contenedor-peliculas").append(`
-       <div class="carousel-cell">
-        <div class="card">
-         <img src="https://image.tmdb.org/t/p/w500${peli.poster_path}" alt="" class="card-img-top">
-         <div class="card-body">
-          <h4 class="card-title">${peli.original_title}</h4>
-          <p class="card-text">
-           ${peli.overview}
-          </p>
-         </div>
-        </div>
-       </div>`);
- });
+  results.forEach((peli) => {
 
- $('#contenedor-peliculas').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
- });
+    let carouselCell = $(`<div class="carousel-cell"></div>`);
+    let card = $(`<div class="card"></div>`);
+    let imagen = $(`<div class="card-img-top imagen-pelicula"></div>`);
+    imagen.css("background-image", `url("https://image.tmdb.org/t/p/w500${peli.poster_path}")`);
+
+    let cardBody = $(`<div class="card-body"></div>`);
+    let cardTitle = $(`<h4 class="card-title">${peli.title}</h4>`);
+
+    let cardText = $(`<p class="card-text"></p>`);
+    let spanTexto = $(`<span>${peli.overview.substr(0, 50)}</span>`);
+    let leerMas = $(`<span class="text-danger">... Leer Mas</span>`);
+
+    cardText.append(spanTexto);
+    cardText.append(leerMas);
+
+    leerMas.click(() => {
+      // capturamis nuevamente al slider de flickity
+      let flick = $('#contenedor-peliculas').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+      });
+
+      let leerMasTexto = leerMas.text();
+      if (leerMasTexto.indexOf("Mas") > -1) {
+        // texto estaba chiquito
+        spanTexto.text(peli.overview);
+        leerMas.text(" Leer Menos");
+        // recalculamos la altura de flickity
+        flick.flickity('resize')
+      } else {
+        // texto estaba largo
+        spanTexto.text(peli.overview.substr(0, 50));
+        leerMas.text("... Leer Mas");
+        // recalculamos la altura de flickity
+        flick.flickity('resize')
+      }
+    })
+
+
+    cardBody.append(cardTitle);
+    cardBody.append(cardText);
+    card.append(imagen);
+    card.append(cardBody);
+
+    carouselCell.append(card);
+
+    $("#contenedor-peliculas").append(carouselCell);
+  });
+
+  $("alertCargando").attr("hidden","hidden");
+  $("#contenedor-peliculas").removeAttr("hidden");
+
+  // Toma al slider de Flickity y y lo convierte en UN SLIDER como tal
+  $('#contenedor-peliculas').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+  });
 }
 
 const traerPeliculas = () => {
- let endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a3175a9a67092b2e56ed90e341fd9bc9";
- fetch(endpoint, { method: 'GET', body: null })
-  .then((response) => {
-   response.json().then((data) => {
-    dibujarPeliculas(data);
-    console.log(data);
-    
-   })
-  })
+  let endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105eb79aa1e6df60a2b95878ad2289aa&language=es";
+  fetch(endpoint, { method: 'GET', body: null })
+    .then((response) => {
+      response.json().then((data) => {
+        dibujarPeliculas(data);
+      })
+    })
 }
 
 traerPeliculas();
--- semana7/dia2/dist/02-peliculas.dev.js
+++ semana7/dia2/dist/02-peliculas.dev.js
@@ -3,8 +3,48 @@
 var dibujarPeliculas = function dibujarPeliculas(_ref) {
   var results = _ref.results;
   results.forEach(function (peli) {
-    $("#contenedor-peliculas").append("\n       <div class=\"carousel-cell\">\n        <div class=\"card\">\n         <img src=\"http://placehold.it/150x150\" alt=\"\" class=\"card-img-top\">\n         <div class=\"card-body\">\n          <h4 class=\"card-title\">".concat(peli.original_title, "</h4>\n          <p class=\"card-text\">\n           ").concat(peli.overview, "\n          </p>\n         </div>\n        </div>\n       </div>"));
-  });
+    var carouselCell = $("<div class=\"carousel-cell\"></div>");
+    var card = $("<div class=\"card\"></div>");
+    var imagen = $("<div class=\"card-img-top imagen-pelicula\"></div>");
+    imagen.css("background-image", "url(\"https://image.tmdb.org/t/p/w500".concat(peli.poster_path, "\")"));
+    var cardBody = $("<div class=\"card-body\"></div>");
+    var cardTitle = $("<h4 class=\"card-title\">".concat(peli.title, "</h4>"));
+    var cardText = $("<p class=\"card-text\"></p>");
+    var spanTexto = $("<span>".concat(peli.overview.substr(0, 50), "</span>"));
+    var leerMas = $("<span class=\"text-danger\">... Leer Mas</span>");
+    cardText.append(spanTexto);
+    cardText.append(leerMas);
+    leerMas.click(function () {
+      // capturamis nuevamente al slider de flickity
+      var flick = $('#contenedor-peliculas').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true
+      });
+      var leerMasTexto = leerMas.text();
+
+      if (leerMasTexto.indexOf("Mas") > -1) {
+        // texto estaba chiquito
+        spanTexto.text(peli.overview);
+        leerMas.text(" Leer Menos"); // recalculamos la altura de flickity
+
+        flick.flickity('resize');
+      } else {
+        // texto estaba largo
+        spanTexto.text(peli.overview.substr(0, 50));
+        leerMas.text("... Leer Mas"); // recalculamos la altura de flickity
+
+        flick.flickity('resize');
+      }
+    });
+    cardBody.append(cardTitle);
+    cardBody.append(cardText);
+    card.append(imagen);
+    card.append(cardBody);
+    carouselCell.append(card);
+    $("#contenedor-peliculas").append(carouselCell);
+  }); // Toma al slider de Flickity y y lo convierte en UN SLIDER como tal
+
   $('#contenedor-peliculas').flickity({
     // options
     cellAlign: 'left',
@@ -13,7 +53,7 @@ var dibujarPeliculas = function dibujarPeliculas(_ref) {
 };
 
 var traerPeliculas = function traerPeliculas() {
-  var endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105eb79aa1e6df60a2b95878ad2289aa";
+  var endpoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105eb79aa1e6df60a2b95878ad2289aa&language=es";
   fetch(endpoint, {
     method: 'GET',
     body: null
