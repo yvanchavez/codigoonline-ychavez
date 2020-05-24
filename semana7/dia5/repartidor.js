2e70a172565aaa034e2942d2b50869e41309c1d8
yvan <chavez.yvan@gmail.com>
2020-05-23 22:38:51 -0500

    crud repartidores terminado

--- semana7/dia5/repartidor.js
+++ semana7/dia5/repartidor.js
@@ -1,5 +1,7 @@
 const URL_BACKEND = "https://5ec8650b155c130016a909e3.mockapi.io";
 formAdd = document.getElementById("formAdd");
+formUpdate = document.getElementById("formUpdate");
+formDelete = document.getElementById("formDelete");
 
 $(document).ready(function () {
   // Activate tooltip
@@ -37,7 +39,7 @@ const getRepartidores = () => {
                   type="checkbox"
                   id="${repartidor.id}"
                   name="options[]"
-                  value="1"
+                  value="${repartidor.id}"
                 />
                 <label for="checkbox1"></label>
               </span>
@@ -46,16 +48,26 @@ const getRepartidores = () => {
             <td>${repartidor.rep_ape}</td>
             <td>${repartidor.rep_dni}</td>
             <td>
-                <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                <a href="#editEmployeeModal" id="${repartidor.id}" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
                 <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
             </td>
           </tr>`
       );
-      // let option = document.createElement("option");
-      // option.innerText = `${repartidor.rep_nom} ${repartidor.rep_ape}`;
-      // option.value = repartidor.id;
-      //option.setAttribute("value",repartidor.id)
-      //selectRepartidor.appendChild(option);
+    });
+
+    //llenar modal editar
+    $("#tabla .edit").on("click", function (e) {
+      console.log(e.currentTarget.id);
+      var row = $(e.currentTarget).parent().parent();
+      var celdas = row.children();
+      let inputNombreUpd = $(celdas[1]).text();
+      let inputApellidoUpd = $(celdas[2]).text();
+      let inputDniUpd = $(celdas[3]).text();
+
+      $("#inputId").val(e.currentTarget.id);
+      $("#inputNombreUpd").val(inputNombreUpd);
+      $("#inputApellidoUpd").val(inputApellidoUpd);
+      $("#inputDniUpd").val(inputDniUpd);
     });
   };
 
@@ -86,7 +98,7 @@ formAdd.onsubmit = (e) => {
 
   console.log(objRepartidor);
 
-   fetch(`${URL_BACKEND}/repartidor`, {
+  fetch(`${URL_BACKEND}/repartidor`, {
     method: "POST",
     body: JSON.stringify(objRepartidor),
     headers: {
@@ -96,7 +108,62 @@ formAdd.onsubmit = (e) => {
     response.json().then((data) => {
       location.reload();
     });
-  }); 
+  });
+
+  $("#addEmployeeModal").modal("hide");
+};
+
+//Actualizar Repartidor
+formUpdate.onsubmit = (e) => {
+  e.preventDefault();
+
+  let inputNombreUpd = document.getElementById("inputNombreUpd").value;
+  let inputApellidoUpd = document.getElementById("inputApellidoUpd").value;
+  let inputDniUpd = document.getElementById("inputDniUpd").value;
+  let id = document.getElementById("inputId").value;
+
+  let objRepartidor = {
+    rep_nom: inputNombreUpd,
+    rep_ape: inputApellidoUpd,
+    rep_dni: inputDniUpd,
+  };
+
+  console.log(objRepartidor);
+
+  fetch(`${URL_BACKEND}/repartidor/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(objRepartidor),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((response) => {
+    response.json().then((data) => {
+      location.reload();
+    });
+  });
+
+  $("#addEmployeeModal").modal("hide");
+};
+
+//Eliminar Repartidor
+formDelete.onsubmit = (e) => {
+  e.preventDefault();
+  let id = $("#tabla input[type='checkbox']:checked").val();
+
+  $("input[type=checkbox]:checked").each(function(){
+    //cada elemento seleccionado
+    fetch(`${URL_BACKEND}/repartidor/${$(this).val()}`, {
+      method: "DELETE",
+    }).then((response) => {
+      response.json().then((data) => {
+        $("#tabla input[type='checkbox']:checked").closest("tr").remove();
+        location.reload();
+      });
+    });
+    
+  });
+
+  
 
   $("#addEmployeeModal").modal("hide");
 };
